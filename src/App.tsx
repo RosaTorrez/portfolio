0741720f573a0e519447737e79c50912e03391e8
rosatorrezquispe <rosatorrezquispe23@gmail.com>
2025-12-04 14:56:17 -0400

    feat: add footer with social links and copyright

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css'
 import About from './components/About'
 import Contact from './components/Contact'
+import Footer from './components/Footer'
 import Header from './components/Header'
 import Hero from './components/Hero'
 import Projects from './components/Projects'
@@ -16,6 +17,7 @@ function App() {
       <Projects />
       <Skills />
       <Contact />
+      <Footer />
     </div>
   )
 }
