92a42f2a991092a680f44b0689b8ecf255c0f7f7
rosatorrezquispe <rosatorrezquispe23@gmail.com>
2025-12-04 14:12:23 -0400

    feta: hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Header from './components/Header'
+import Hero from './components/Hero'
 
 function App() {
 
   return (
     <div className="min-h-screen bg-background text-foreground">
       <Header />
+      <Hero />
     </div>
   )
 }
