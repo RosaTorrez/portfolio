213f478a42f58d5524d02c63893e9fc4667727bf
RosaTorrez <rosatorrezquispe23@gmail.com>
2025-12-04 14:45:26 -0400

    feat: implement skills section with progress bars (#6)

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import About from './components/About'
 import Header from './components/Header'
 import Hero from './components/Hero'
 import Projects from './components/Projects'
+import Skills from './components/Skills'
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
       <Hero />
       <About />
       <Projects />
+      <Skills />
     </div>
   )
 }
