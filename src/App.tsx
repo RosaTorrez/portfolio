cd942e50274e3b409001010bd73380447450bf4e
RosaTorrez <rosatorrezquispe23@gmail.com>
2025-12-04 14:35:00 -0400

    Feature/projects (#5)
    * feat: updated README file for proyect structure and example
    
    * Feature/hero (#3)
    
    * Feature/header (#1)
    
    * feat: updated README file for proyect structure and example
    
    * feat: header added
    
    * feat: updated README file for proyect structure and example (#2)
    
    * feta: hero section
    
    * feat: implement about section with experience timeline (#4)
    
    * feat: add projects gallery and project card component
    
    * feat: add projects gallery and project card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import './App.css'
+import About from './components/About'
 import Header from './components/Header'
+import Hero from './components/Hero'
+import Projects from './components/Projects'
 
 function App() {
 
   return (
     <div className="min-h-screen bg-background text-foreground">
       <Header />
+      <Hero />
+      <About />
+      <Projects />
     </div>
   )
 }
