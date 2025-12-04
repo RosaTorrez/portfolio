032a49c00e2326a74a5056414c10d8f9b055aee9
RosaTorrez <rosatorrezquispe23@gmail.com>
2025-12-04 14:26:32 -0400

    feat: implement about section with experience timeline (#4)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+import About from './components/About'
 import Header from './components/Header'
 import Hero from './components/Hero'
 
@@ -8,6 +9,7 @@ function App() {
     <div className="min-h-screen bg-background text-foreground">
       <Header />
       <Hero />
+      <About />
     </div>
   )
 }
